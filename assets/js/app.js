46af139837754ef420d42f14bc0b1d95b6bf768f
Joelle Sim <1306118i@student.tp.edu.sg>
2015-11-19 15:36:15 +0800

    Vibration for 1 second added

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,16 @@ $(document).ready(function () {
 	$(".joelle-btn").click(function () {
 		$('#greeting').html('Beacon detected!!!');
 	});
+	
+	$(".vibrate-btn").click(function(){
+		//// enable vibration support
+		//navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+
+		if (navigator.vibrate) {
+		// vibration API supported
+		
+		// vibrate for one second
+		navigator.vibrate(1000);
+		}
+	});
 });
