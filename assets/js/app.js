957b8d51901bac7016e586e669a0b2c6de8f1a35
Joelle Sim <1306118i@student.tp.edu.sg>
2015-11-19 16:45:35 +0800

    redirect pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
 	$(".joelle-btn").click(function () {
 		$('#greeting').html('Beacon detected!!!');
+		$('#greeting').css("color", "#FF0000");
 	});
+	
+	$(".vibratepage-btn").click(function () {
+		$('#greeting').html('Redirect to vibrate page');
+		$('#greeting').css("color", "#FF0000");
+     window.location.href = "vibrate.html";
+    });
 	
 	$(".vibrate-btn").click(function(){
 		// enable vibration support
@@ -11,9 +18,7 @@ $(document).ready(function () {
 		// vibration API supported
 		
 		// vibrate for 60 second
-		navigator.vibrate(60000);
-		
-			
+		navigator.vibrate(60000);	
 		}else{
 			$('#greeting').html('No Vibration is allowed!!!');
 		}
