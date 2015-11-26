aa449a9ebf6d3cf4f5d7b574fe6d25425160191e
Joelle Sim <1306118i@student.tp.edu.sg>
2015-11-26 11:57:25 +0800

    added in ranging beacons code and plugin

---
+++ assets/js/beacon.js
@@ -0,0 +1,21 @@
+var region = { identifier: 'MyRegion' }
+
+EstimoteBeacons.startRangingBeaconsInRegion(
+   region,
+   onBeaconsRanged,
+   onError)
+
+$(".range-btn").click(function () {
+	function onBeaconsRanged(beaconInfo)
+	{
+		//console.log('Number of Estimote Beacons ranged: ' +
+		//beaconInfo.beacons.length)
+		$('#beacontitle').html("Number of Estimote Beacons ranged: "+ beaconInfo.beacons.length);	   
+	}
+	
+	function onError(error)
+	{
+		//console.log('Start ranging error: ' + error)
+		$('#beacontitle').html("Start ranging error: " + error)
+	}
+});
